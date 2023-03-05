443d0365c7a64703aecb63c4d9c94381aacee7e5
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2023-03-05 15:37:41 -0300

    [WIP]-creating-abilities-token-to-employee-and-employerr

--- back-end/laravel/routes/api.php
+++ back-end/laravel/routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\EmploymentController;
 
 /*
@@ -17,19 +18,20 @@ use App\Http\Controllers\EmploymentController;
 |
 */
 
+Route::resources(['employments' => EmploymentController::class,])->only(['index']);
+
 Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::resources([
         'employments' => EmploymentController::class,
     ]);
-
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
+    Route::post('register', [EmployeeController::class, 'register']);
+    Route::post('employer/register', [EmployerController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::middleware('auth:sanctum')->delete('logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('user', [AuthController::class, 'user']);
+        Route::delete('logout', [AuthController::class, 'logout']);
+    });
 });
