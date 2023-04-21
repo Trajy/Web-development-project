d2a1dbda415e7a7c37a0975e6cc281341c025ef9
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-04-21 12:38:32 -0300

    created-method-definitionWithType-in-UserFactory

--- back-end/laravel/database/factories/UserFactory.php
+++ back-end/laravel/database/factories/UserFactory.php
@@ -4,12 +4,14 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+
     /**
      * Define the model's default state.
      *
@@ -21,9 +23,16 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => '1234', // password
+            'remember_token' => Str::random(10)
         ];
+    }
+
+    public function definitionWithType(string $type)
+    {
+        $definition = $this->definition();
+        $definition['type'] = $type;
+        return $this->state(fn ($atributes) => $definition);
     }
 
     /**
