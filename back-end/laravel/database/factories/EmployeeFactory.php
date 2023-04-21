6e614d3d06724aaf448800a613c374d0510df005
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-04-21 12:39:39 -0300

    created-EmployeeFactory-and-EmployeeTableSeeder

---
+++ back-end/laravel/database/factories/EmployeeFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+
+    private const GENERATE_WITH_MASK = false;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->firstName(),
+            'surname' => fake()->lastName(),
+            'cpf' => fake()->cpf($this::GENERATE_WITH_MASK),
+            'user_id' => User::factory()->definitionWithType('employer')->create()->id
+        ];
+    }
+}
